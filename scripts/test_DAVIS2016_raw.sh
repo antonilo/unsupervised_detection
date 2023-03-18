b07fc9f53f58f16e2f12f29beed2c4a8308c01b5
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-03-18 17:08:37 +0900

    update davis test script to ready for run.

--- scripts/test_DAVIS2016_raw.sh
+++ scripts/test_DAVIS2016_raw.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-
+#
 # Script to compute raw results (before post-processing)
+###
 
-CKPT_FILE='/path/to/davis2016_ckpt/'
-DATASET_FILE='/path/to/DAVIS_2016/'
-PWC_CKPT_FILE='/path/to/pwc_ckpt/'
+# download data
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd ${SCRIPT_DIR}/..
+mkdir -p downloads
+# (TODO) wget DAVIS etc. to downloads/
 
+# set params
+CKPT_FILE='downloads/unsupervised_detection_models/davis_best_model/model.best'
+DATASET_FILE='downloads/DAVIS/'
+PWC_CKPT_FILE='downloads/pwcnet-lg-6-2-multisteps-chairsthingsmix/pwcnet.ckpt-595000.data-00000-of-00001'
+
+# run test
 python3 test_generator.py \
 --dataset=DAVIS2016 \
 --ckpt_file=$CKPT_FILE \
