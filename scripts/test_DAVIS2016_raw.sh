00ceff328a07a78aef9c026203c3af452f9a96c0
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-03-19 12:40:41 +0900

    update test_DAVIS script to automatically download data.

--- scripts/test_DAVIS2016_raw.sh
+++ scripts/test_DAVIS2016_raw.sh
@@ -3,18 +3,35 @@
 # Script to compute raw results (before post-processing)
 ###
 
-# download data
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
-cd ${SCRIPT_DIR}/..
-mkdir -p downloads
-# (TODO) wget DAVIS etc. to downloads/
 
-# set params
-CKPT_FILE='downloads/unsupervised_detection_models/davis_best_model/model.best'
-DATASET_FILE='downloads/DAVIS/'
-PWC_CKPT_FILE='downloads/pwcnet-lg-6-2-multisteps-chairsthingsmix/pwcnet.ckpt-595000.data-00000-of-00001'
+# parameters
+DOWNLOAD_DIR="${SCRIPT_DIR}/../download"
+CKPT_FILE="${DOWNLOAD_DIR}/unsupervised_detection_models/davis_best_model/model.best"
+DATASET_FILE="${DOWNLOAD_DIR}/DAVIS"
+PWC_CKPT_FILE="${DOWNLOAD_DIR}/pwcnet-lg-6-2-multisteps-chairsthingsmix/pwcnet.ckpt-595000.data-00000-of-00001"
+RESULT_DIR="{SCRIPT_DIR}/../results"
 
-# run test
+
+echo "[INFO] start downloading data..."
+mkdir -p ${DOWNLOAD_DIR}
+if [ ! -f ${CKPT_FILE}.data* ]; then
+    echo "[INFO] no checkpoint file found. start downloading it."
+    wget https://rpg.ifi.uzh.ch/data/unsupervised_detection_models.zip
+    unzip unsupervised_detection_models.zip
+    rm unsupervised_detection_models.zip
+fi
+if [ ! -e ${DATASET_FILE} ]; then
+    echo "[INFO] no DAVIS data found. start downloading it."
+    wget https://graphics.ethz.ch/Downloads/Data/Davis/DAVIS-data.zip
+    unzip DAVIS-data.zip
+    rm DAVIS-data.zip
+fi
+echo "[INFO] finished downloading."
+
+
+echo "[INFO] start running a test..."
+mkdir -p ${RESULT_DIR}
 python3 test_generator.py \
 --dataset=DAVIS2016 \
 --ckpt_file=$CKPT_FILE \
@@ -23,4 +40,5 @@ python3 test_generator.py \
 --test_temporal_shift=1 \
 --root_dir=$DATASET_FILE \
 --generate_visualization=True \
---test_save_dir=/tmp/davis_test
+--test_save_dir=${RESULT_DIR}/davis_test
+echo "[INFO] finished the test."
